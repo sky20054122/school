1ccac73cbaf58fd8af1b4109caf6f7868bc55850
sky20054122 <yangxiaobing520@163.com>
2017-07-25 16:43:42 +0800

    修改为spring boot 1.5.4 add spring.session.store-type=none

--- src/test/java/com/brxy/SchoolApplicationTests.java
+++ src/test/java/com/brxy/SchoolApplicationTests.java
@@ -2,12 +2,12 @@ package com.brxy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SchoolApplication.class)
+@SpringBootApplication
 @WebAppConfiguration
 public class SchoolApplicationTests {
 
